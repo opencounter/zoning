60626495f1dc77a2aabc1451001588aaab348375
Joel Mahoney <joelmahoney@gmail.com>
2014-09-13 17:35:22 -0700

    Don't require subdomain and locale on geocode method

--- lib/zoning/endpoints/geo.rb
+++ lib/zoning/endpoints/geo.rb
@@ -4,10 +4,10 @@ module Zoning
 		# Geo.geocode takes the folling, required params:
 		# :address, String
 
-		def self.geocode(subdomain, locale, query={})
+		def self.geocode(query={})
 			key = 'addresses'
 			query_string = {q: query}.to_query
-			connection = Zoning::Connection.connect(subdomain, locale, "geocode.json", query_string).get
+			connection = Zoning::Connection.connect(nil, :en, "geocode.json", query_string).get
 			Zoning::Connection.parse(connection, key)
 		end
 	end
