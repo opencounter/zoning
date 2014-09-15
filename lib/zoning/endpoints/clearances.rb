8ffffd7d9363d4b1d52df689cbf0b357e7eb0d32
Joel Mahoney <joelmahoney@gmail.com>
2014-09-15 06:38:40 -0700

    Change Clearance.find to Clearance.search

--- lib/zoning/endpoints/clearances.rb
+++ lib/zoning/endpoints/clearances.rb
@@ -1,12 +1,12 @@
 module Zoning
 	module Clearances
-		
-		def self.find(subdomain, locale, query={})
+
+		def self.search(subdomain, locale, query={})
 			key = 'clearance'
 			query_string = {q: query}.to_query
 			connection = Zoning::Connection.connect(subdomain, locale, "clearance.json", query_string).get
 			Zoning::Connection.parse(connection, key)
 		end
-	
+
 	end
 end
