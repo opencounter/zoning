3e81684715082455e37d14a1bceb9f1fd939362e
Joel Mahoney <joelmahoney@gmail.com>
2014-09-16 07:52:42 -0700

    Include subdomain on geocode lookup (to scope to city)

--- lib/zoning/endpoints/geo.rb
+++ lib/zoning/endpoints/geo.rb
@@ -4,7 +4,7 @@ module Zoning
 		# Geo.geocode takes the folling, required params:
 		# :address, String
 
-		def self.geocode(query={})
+		def self.geocode(subdomain, query={})
 			key = 'addresses'
 			query_string = {q: query}.to_query
 			connection = Zoning::Connection.connect(nil, :en, "geocode.json", query_string).get
