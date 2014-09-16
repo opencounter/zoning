2349fb30daf8244abe86214bb53b80b9e864e5da
Joel Mahoney <joelmahoney@gmail.com>
2014-09-16 16:19:31 -0700

    Pass key to attributes search method (return an array not a hash)

--- lib/zoning/endpoints/attributes.rb
+++ lib/zoning/endpoints/attributes.rb
@@ -2,10 +2,11 @@ module Zoning
 	module Attributes
 
 		def self.search(subdomain, locale, query={})
+			key = 'attributes'
 			query_string = {q: query}.to_query
 			connection = Zoning::Connection.connect(subdomain, locale, "attributes/search.json", query_string).get
-			Zoning::Connection.parse(connection)
+			Zoning::Connection.parse(connection, key)
 		end
-		
+
 	end
 end
