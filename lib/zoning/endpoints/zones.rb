d0122bbeed26d53f790d878a541521e37d326dd7
Joel Mahoney <joelmahoney@gmail.com>
2014-11-08 22:43:12 -0800

    Add search method to Zones endpoint

--- lib/zoning/endpoints/zones.rb
+++ lib/zoning/endpoints/zones.rb
@@ -8,14 +8,21 @@ module Zoning
 			Zoning::Connection.parse(connection, key)
 		end
 
-    ALLOWED_SEARCH_PARAMS = %i(id code name description latitude longitude overlay)
-		
+    ALLOWED_SEARCH_PARAMS = %i(id slug code name description latitude longitude overlay)
+
 		def self.list(subdomain, locale, query={})
 			key = 'zones'
 			query_string = Faraday::Utils::ParamsHash.new.merge({q: query}).to_query
 			connection = Zoning::Connection.connect(subdomain, locale, "zones.json", query_string).get
 			Zoning::Connection.parse(connection, key)
-
 		end
+
+		def self.search(subdomain, locale, keywords)
+			key = 'zones'
+			query_string = Faraday::Utils::ParamsHash.new.merge({q: keywords}).to_query
+			connection = Zoning::Connection.connect(subdomain, locale, "zones/search.json", query_string).get
+			Zoning::Connection.parse(connection, key)
+		end
+
 	end
 end
