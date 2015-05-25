e2a9f586ba3f8fcacb10b9ee3de30621954da3f6
Joel Mahoney <joelmahoney@gmail.com>
2015-05-25 15:25:59 -0700

    Enable geojson format on Zones.list

--- lib/zoning_api/endpoints/zones.rb
+++ lib/zoning_api/endpoints/zones.rb
@@ -1,10 +1,10 @@
 module ZoningAPI
 	module Zones
 
-		def self.find(subdomain, locale, id, options={})
+		def self.find(subdomain, locale, id, format=nil)
 			key = 'zone'
-			if options[:format].present? && ['json', 'geojson'].include?(options[:format])
-				format = options[:format]
+			if format.present? && ['json', 'geojson'].include?(format)
+				format
 			else
 				format = 'json'				
 			end
@@ -12,10 +12,15 @@ module ZoningAPI
 			ZoningAPI::Connection.parse(connection, key)
 		end
 
-		def self.list(subdomain, locale, query={})
+		def self.list(subdomain, locale, query={}, format=nil)
 			key = 'zones'
+			if format.present? && ['json', 'geojson'].include?(format)
+				format
+			else
+				format = 'json'				
+			end
 			query_string = Faraday::Utils::ParamsHash.new.merge(query).to_query
-			connection = ZoningAPI::Connection.connect(subdomain, locale, "zones.json", query_string).get
+			connection = ZoningAPI::Connection.connect(subdomain, locale, "zones.#{format}", query_string).get
 			ZoningAPI::Connection.parse(connection, key)
 		end
 
